3f294d4387cbf3e04e4f108961f4a1bfd6c2886d
nicktamura <67190316+nicktamura@users.noreply.github.com>
2020-07-15 15:15:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var name = prompt("Introduce your name");
+if (name==="Jess" || name==="jess"){
+  alert("Hello " +name+"! Hope you are wearing your special boot. Silly billy");
+} else {
+  alert("Hello " +name+"!");
+}
